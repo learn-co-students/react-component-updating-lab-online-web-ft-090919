df794d2cdef940b0ba422a34d073c4aac0a6cd00
bigcatplichta <bigcatplichta@gmail.com>
2020-02-09 21:50:03 -0700

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 
-class Timer extends Component {
+class Timer extends PureComponent {
   constructor() {
     super();
     this.timer = React.createRef();
@@ -12,6 +12,11 @@ class Timer extends Component {
 
   //Your code here
 
+  componentDidUpdate() {
+    this.timer.current.style.color =
+  "#" + Math.floor(Math.random() * 16777215).toString(16)
+  }
+
   componentDidMount() {
     this.interval = setInterval(
       this.clockTick,
@@ -21,6 +26,13 @@ class Timer extends Component {
 
   componentWillUnmount() {
     clearInterval(this.interval);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
   }
 
   render() {
